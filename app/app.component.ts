dcb4d9afcd9419358107499f4102a30694786443
ctevnan <carolyntevnan@yahoo.com>
2016-09-22 16:30:48 -0400

    stuck on loading screen

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ export class Hero {
 
 @Component({
   selector: 'my-app',
-  template: '
+  template: `
     <h1>{{title}}</h1>
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
@@ -15,12 +15,12 @@ export class Hero {
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name">
     </div>
-    '  
+    `  
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
-  };
+    name: 'windstorm'
+  };  
 }
