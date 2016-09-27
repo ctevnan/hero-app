de30caf18c79d83d5a3c3230d814012391b348f8
ctevnan <carolyntevnan@yahoo.com>
2016-09-27 15:39:04 -0400

    migrating template to its own file hero-detail.component.html, adding go back button

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 const appRoutes: Routes = [
   {
@@ -14,6 +15,10 @@ const appRoutes: Routes = [
     path: 'dashboard',
     component: DashboardComponent
   },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },  
   {
     path: 'heroes',
     component: HeroesComponent  
