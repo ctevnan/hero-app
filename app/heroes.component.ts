a5245a04e9d66074a555b74c4cf535742fd73f61
ctevnan <carolyntevnan@yahoo.com>
2016-09-28 11:30:44 -0400

    revised component class

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import  { HeroService } from './hero.service';
@@ -7,15 +8,15 @@ import  { HeroService } from './hero.service';
   moduleId: module.id,
   selector: 'my-heroes',
   templateUrl: 'heroes.component.html',
-  styleUrls: [ 'heroes.component.css' ]
-  providers: [HeroService]    
+  styleUrls: [ 'heroes.component.css' ]    
 })
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   heroes = Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -27,5 +28,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }  
 }
