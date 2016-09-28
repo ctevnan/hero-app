f4cd8e73057c3dd4a968b250ce77d644bf544931
ctevnan <carolyntevnan@yahoo.com>
2016-09-28 14:18:21 -0400

    converting getHeroes() to use HTTP

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,17 +1,22 @@
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
-  }
 
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise<Hero[]>(resolve =>
-      setTimeout(resolve, 2000)) //delay 2 seconds
-      .then(() => this.getHeroes());
+  private heroesUrl = 'app/heroes'; // URL to web api
+
+  constructor(private http: Http) { }
+  
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+        .toPromise()
+        .then(response => response.json().data as Hero[])
+        .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
