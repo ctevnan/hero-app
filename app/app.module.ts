1839dd6a784c943a8b2f22dd9a8074b193c2d4e7
ctevnan <carolyntevnan@yahoo.com>
2016-09-28 13:37:46 -0400

    importing HttpModule to app.module.ts, also adding a few commas that I missed

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
-import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { routing } from './app.routing';
 
@@ -13,16 +14,17 @@ import { routing } from './app.routing';
   imports: [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
   ],
   providers: [
-    HeroService
+    HeroService,
   ],
   bootstrap: [ AppComponent ]
 })
