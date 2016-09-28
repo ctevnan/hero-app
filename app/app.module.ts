851416bcdfed51d94d2619cc55936771b8a33848
ctevnan <carolyntevnan@yahoo.com>
2016-09-28 12:46:50 -0400

    small changes to caps, indentation

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
@@ -26,4 +27,4 @@ import { routing } from './app.routing';
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
- }
+}
